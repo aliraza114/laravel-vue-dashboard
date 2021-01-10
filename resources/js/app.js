fd75fbf35da5de32c9064bc3690d1ea637a0eea7
aliraza35690 <aliraza3569@gmail.com>
2021-01-10 21:01:38 +0500

    role base testing

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,75 +10,65 @@ window.Vue = require('vue');
 import moment from 'moment';
 import { Form, HasError, AlertError } from 'vform';
 
-import swal from 'sweetalert2';
+import Gate from "./Gate";
+Vue.prototype.$gate = new Gate(window.user);
+
+
+import swal from 'sweetalert2'
 window.swal = swal;
 
-const Toast = Swal.mixin({
-    toast: true,
-    position: 'top-end',
-    showConfirmButton: false,
-    timer: 3000,
-    timerProgressBar: true,
-    didOpen: (toast) => {
-        toast.addEventListener('mouseenter', Swal.stopTimer)
-        toast.addEventListener('mouseleave', Swal.resumeTimer)
-    }
-})
+const toast = swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000
+});
 
-window.toast = Toast
+window.toast = toast;
 
 
 window.Form = Form;
 Vue.component(HasError.name, HasError)
 Vue.component(AlertError.name, AlertError)
 
+Vue.component('pagination', require('laravel-vue-pagination'));
 
 
 import VueRouter from 'vue-router'
-
 Vue.use(VueRouter)
+
 import VueProgressBar from 'vue-progressbar'
 Vue.use(VueProgressBar, {
     color: 'rgb(143, 255, 199)',
     failedColor: 'red',
-    height: '2px'
-})
+    height: '3px'
+  })
 
 let routes = [
     { path: '/dashboard', component: require('./components/Dashboard.vue') },
+    { path: '/developer', component: require('./components/Developer.vue') },
     { path: '/users', component: require('./components/Users.vue') },
-    { path: '/profile', component: require('./components/Proflie.vue') }
-]
-
+    { path: '/profile', component: require('./components/Profile.vue') },
+    { path: '*', component: require('./components/NotFound.vue') }
+  ]
 
 const router = new VueRouter({
     mode: 'history',
     routes // short for `routes: routes`
-})
-
-Vue.filter('upText', function (text) {
-    return text.charAt(0).toUpperCase() + text.slice(1);
-})
-
-Vue.filter('date', function (created) {
-    return moment(created).format('MMMM DD YYYY');
-})
-
-window.Fire = new Vue();
+  })
 
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+Vue.filter('upText', function(text){
+    return text.charAt(0).toUpperCase() + text.slice(1)
+});
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.filter('myDate',function(created){
+    return moment(created).format('MMMM Do YYYY');
+});
+
+
+window.Fire =  new Vue();
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -86,7 +76,43 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
+
+Vue.component(
+    'passport-clients',
+    require('./components/passport/Clients.vue')
+);
+
+Vue.component(
+    'passport-authorized-clients',
+    require('./components/passport/AuthorizedClients.vue')
+);
+
+Vue.component(
+    'passport-personal-access-tokens',
+    require('./components/passport/PersonalAccessTokens.vue')
+);
+
+Vue.component(
+    'not-found',
+    require('./components/NotFound.vue')
+);
+
+
+Vue.component('example-component', require('./components/ExampleComponent.vue'));
+
 const app = new Vue({
     el: '#app',
-    router
+    router,
+    data:{
+        search: ''
+    },
+    methods:{
+        searchit: _.debounce(() => {
+            Fire.$emit('searching');
+        },1000),
+
+        printme() {
+            window.print();
+        }
+    }
 });
